55d10c02fcd1fd9a1586de4315682ebbaac51dc2
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-12 17:46:52 +0000

    updated API name for consistency with tweepy docs

--- 02_twitter_api_access_01/01_tweet_access.py
+++ 02_twitter_api_access_01/01_tweet_access.py
@@ -3,17 +3,16 @@ import json
 from tweepy import OAuthHandler
 from twitter import twitter_api, get_auth
 
-api = twitter_api()
-
 auth = get_auth()
 
-api = tweepy.API(auth)
+API = tweepy.API(auth)
 
 count = 10
 query = "Dublin"
 
 # Get all status
-results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+results = [status for status in tweepy.Cursor(
+    API.search_tweets, q=query).items(count)]
 
 for result in results:
-    print (json.dumps(result._json, indent=2))
+    print(json.dumps(result._json, indent=2))
