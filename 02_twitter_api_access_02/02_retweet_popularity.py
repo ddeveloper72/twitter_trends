8eb3200148ee95e7138e35acc6c780903141371d
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-12 18:41:44 +0000

    updated tweepy API search tweets

--- 02_twitter_api_access_02/02_retweet_popularity.py
+++ 02_twitter_api_access_02/02_retweet_popularity.py
@@ -14,18 +14,19 @@ count = 10
 query = "#allirelandfinal"
 
 # get all tweets for the search query
-results = [status for status in tweepy.Cursor(API.search, q=query).items(count)]
+results = [status for status in tweepy.Cursor(
+    API.search_tweets, q=query).items(count)]
 
 min_retweets = 10   # The minimum number of times a status is retweeted to gain
-                    # entry into our list.  We change this to suit our own purposes.
+# entry into our list.  We change this to suit our own purposes.
 
 pop_tweets = [status
-                for status in results
-                    if status._json['retweet_count'] > min_retweets]
+              for status in results
+              if status._json['retweet_count'] > min_retweets]
 
 # Create a list of tweet-tuples, associating each tweet's text with it's tweet count
-tweet_tups = [(tweet._json['text'].encode('utf-8'),tweet._json['retweet_count'])
-                for tweet in pop_tweets]
+tweet_tups = [(tweet._json['text'].encode('utf-8'), tweet._json['retweet_count'])
+              for tweet in pop_tweets]
 
 # Sort the tuple entries into descending order
 most_popular_tups = sorted(tweet_tups, key=itemgetter(1), reverse=True)[:5]
@@ -34,6 +35,6 @@ most_popular_tups = sorted(tweet_tups, key=itemgetter(1), reverse=True)[:5]
 table = PrettyTable(field_names=['Text', 'Retweet Count'])
 for key, val in most_popular_tups:
     table.add_row([key, val])
-table.max_width['Text']=50
-table.align['Text'],table.align['Retweet Count']= 'l', 'r' # align columns
+table.max_width['Text'] = 50
+table.align['Text'], table.align['Retweet Count'] = 'l', 'r'  # align columns
 print(table)
