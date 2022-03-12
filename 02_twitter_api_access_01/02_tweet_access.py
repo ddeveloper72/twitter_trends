b87495fd9e2615642a6d577ecff0e34d99dc22d3
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-12 17:50:20 +0000

    updated tweepy API search tweets

--- 02_twitter_api_access_01/02_tweet_access.py
+++ 02_twitter_api_access_01/02_tweet_access.py
@@ -3,36 +3,32 @@ import json
 from tweepy import OAuthHandler
 from twitter import twitter_api, get_auth
 
-api = twitter_api()
-
 auth = get_auth()
 
-api = tweepy.API(auth)
+API = tweepy.API(auth)
 
 count = 50
 query = "Dublin"
 
 # Get all status
-results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+results = [status for status in tweepy.Cursor(
+    API.search_tweets, q=query).items(count)]
 
 status_texts = [status._json['text'] for status in results]
 
-screen_names = [status._json['user']['scrren_name']
-                                for status in results
-                                    for mention in status._json['entities']['user_mentions']]
+screen_names = [status._json['user']['screen_name']
+                for status in results
+                for mention in status._json['entities']['user_mentions']]
 
 hashtags = [hashtag['text']
-                                for status in results
-                                    for hashtag in status._json['entaties']['hashtags']]
-                                    
+            for status in results
+            for hashtag in status._json['entities']['hashtags']]
+
 words = [word
-                        for text in status_texts
-                            for word in text.split()]
+         for text in status_texts
+         for word in text.split()]
 
-print(json.dumps(status_texts[0:5], indent = 1))
-print(json.dumps(screen_names[0:5], indent = 1))
-print(json.dumps(hashtags[0:5], indent = 1))
-print(json.dumps(words[0:5], indent = 1))
-
-                            
-            
+print(json.dumps(status_texts[0:5], indent=1))
+print(json.dumps(screen_names[0:5], indent=1))
+print(json.dumps(hashtags[0:5], indent=1))
+print(json.dumps(words[0:5], indent=1))
