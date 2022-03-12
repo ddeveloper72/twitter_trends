8c2294b89d29fa5443566e0d277aee48ab549874
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-12 17:03:27 +0000

    updated tweepy API get place trends

--- 01_twitter_api_intro_01/02_twitter_intro.py
+++ 01_twitter_api_intro_01/02_twitter_intro.py
@@ -3,29 +3,27 @@ import json
 from tweepy import OAuthHandler
 from twitter import twitter_api, get_auth
 
-api = twitter_api()
-
 auth = get_auth()
 
-api = tweepy.API(auth)
+API = tweepy.API(auth)
 
 DUB_WOE_ID = 560743
 LON_WOE_ID = 44418
 CTN_WOE_ID = 1591691
 
-dub_trends = api.trends_place(DUB_WOE_ID)
-lon_trends = api.trends_place(LON_WOE_ID)
-ctn_trends = api.trends_place(CTN_WOE_ID)
+lon_trends = API.get_place_trends(LON_WOE_ID)
+ctn_trends = API.get_place_trends(CTN_WOE_ID)
+dub_trends = API.get_place_trends(DUB_WOE_ID)
 
 dub_trends_set = set([trend['name']
-                    for trend in dub_trends[0]['trends']])
-                    
+                      for trend in dub_trends[0]['trends']])
+
 lon_trends_set = set([trend['name']
-                    for trend in lon_trends[0]['trends']])
-                    
+                      for trend in lon_trends[0]['trends']])
+
 ctn_trends_set = set([trend['name']
-                    for trend in ctn_trends[0]['trends']])                
-                    
+                      for trend in ctn_trends[0]['trends']])
+
 common_trends = set.intersection(dub_trends_set, lon_trends_set)
 
 print(common_trends)
