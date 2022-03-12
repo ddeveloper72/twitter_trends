213c6f4733a503b9bc0c0ede0386b4d401237be9
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-12 17:55:48 +0000

    updated tweepy API search tweets

--- 02_twitter_api_access_01/04_tweet_access.py
+++ 02_twitter_api_access_01/04_tweet_access.py
@@ -13,32 +13,29 @@ count = 50
 query = "Weather"
 
 # Get all status
-results = [status for status in tweepy.Cursor(API.search, q=query).items(count)]
+results = [status for status in tweepy.Cursor(
+    API.search_tweets, q=query).items(count)]
 
 status_texts = [status._json['text'] for status in results]
 
 screen_names = [status._json['user']['screen_name']
-                                for status in results
-                                    for mention in status._json['entities']['user_mentions']]
+                for status in results
+                for mention in status._json['entities']['user_mentions']]
 
 hashtags = [hashtag['text']
-                                for status in results
-                                    for hashtag in status._json['entities']['hashtags']]
-                                    
+            for status in results
+            for hashtag in status._json['entities']['hashtags']]
+
 words = [word
-                        for text in status_texts
-                            for word in text.split()]
+         for text in status_texts
+         for word in text.split()]
 
-for lable, data in (('Text', status_texts),
-                        ('Screen Name', screen_names),
-                        ('Word', words)):
-        table = PrettyTable(field_names=[lable, 'Count'])
-        counter = Counter(data)
-        [table.add_row(entry) for entry in counter.most_common()[:10]]
-        table.align[lable], table.align['Count'] = 'l', 'r' # left & right align the columns
-        print(table)
-                        
-
-
-                            
-            
+for label, data in (('Text', status_texts),
+                    ('Screen Name', screen_names),
+                    ('Word', words)):
+    table = PrettyTable(field_names=[label, 'Count'])
+    counter = Counter(data)
+    [table.add_row(entry) for entry in counter.most_common()[:10]]
+    # left & right align the columns
+    table.align[label], table.align['Count'] = 'l', 'r'
+    print(table)
