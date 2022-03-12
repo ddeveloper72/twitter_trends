18006f6ff8ec35d67969354444fe5edc3ac2eaca
ddeveloper72 <duncan.niamh@gmail.com>
2022-03-12 17:53:54 +0000

    updated tweepy API search tweets

--- 02_twitter_api_access_01/03_tweet_access.py
+++ 02_twitter_api_access_01/03_tweet_access.py
@@ -4,36 +4,31 @@ from tweepy import OAuthHandler
 from collections import Counter
 from twitter import twitter_api, get_auth
 
-api = twitter_api()
-
 auth = get_auth()
 
-api = tweepy.API(auth)
+API = tweepy.API(auth)
 
 count = 50
 query = "Weather"
 
 # Get all status
-results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+results = [status for status in tweepy.Cursor(
+    API.search_tweets, q=query).items(count)]
 
 status_texts = [status._json['text'] for status in results]
 
 screen_names = [status._json['user']['screen_name']
-                                for status in results
-                                    for mention in status._json['entities']['user_mentions']]
+                for status in results
+                for mention in status._json['entities']['user_mentions']]
 
 hashtags = [hashtag['text']
-                                for status in results
-                                    for hashtag in status._json['entities']['hashtags']]
-                                    
+            for status in results
+            for hashtag in status._json['entities']['hashtags']]
+
 words = [word
-                        for text in status_texts
-                            for word in text.split()]
+         for text in status_texts
+         for word in text.split()]
 
 for entry in [screen_names, hashtags, words]:
     counter = Counter(entry)
-    print(counter.most_common()[:10]) # the top 10 results
-
-
-                            
-            
+    print(counter.most_common()[:10])  # the top 10 results
